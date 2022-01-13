afb7ace366aaf9581d2df9622d2d2b61c7c35e84
Sai Varun Teja Mudumba <samudumb@amazon.com>
2022-01-13 13:17:17 -0800

    Fix java/aws-inefficient-chain@v1.0

--- src/java/rules/aws_inefficient_chain/AwsInefficientChain.java
+++ src/java/rules/aws_inefficient_chain/AwsInefficientChain.java
@@ -10,13 +10,20 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 
+import java.io.IOException;
+
 public class AwsInefficientChain {
     // {fact rule=aws-inefficient-chain@v1.0 defects=1}
-    private String inefficientApiCallsNonCompliant(final String bucketName, final String key) {
+    private String inefficientApiCallsNonCompliant(final String bucketName, final String key) throws IOException {
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
         // Noncompliant: uses inefficient chain of API calls over an efficient single API call.
         S3Object s3object = s3Client.getObject(bucketName, key);
-        return s3object.getObjectMetadata().getVersionId();
+        try {
+            return s3object.getObjectMetadata().getVersionId();
+        } finally {
+            s3object.close();
+        }
+
     }
     // {/fact}
 
