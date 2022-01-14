3582cbdc179e9926e84af9f9bdf45ef8d42628a8
Sai Varun Teja Mudumba <samudumb@amazon.com>
2022-01-13 21:49:37 -0800

    Fix detections

--- src/java/rules/lambda_client_reuse/LambdaClientReuseNoncompliant.java
+++ src/java/rules/lambda_client_reuse/LambdaClientReuseNoncompliant.java
@@ -6,38 +6,28 @@
 package rules.lambda_client_reuse;
 
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.AmazonS3URI;
 
 // {fact rule=lambda-client-reuse@v1.0 defects=1}
-public class LambdaClientReuseNonCompliant implements RequestHandler<String, Void> {
+public class LambdaClientReuseNoncompliant implements RequestHandler<String, Void> {
 
     private AmazonS3 s3Client;
 
 
     public Void handleRequest(String requestEvent, Context context) {
         System.err.println("Nothing to see here");
-        createBucketNonCompliant();
-        insertObjectNonCompliant("storeOject");
+        createBucketNoncompliant();
         return null;
     }
 
-    private void createBucketNonCompliant() {
+    private void createBucketNoncompliant() {
         s3Client.createBucket("bucketName");
         // Noncompliant: recreates AWS clients in each lambda invocation.
         this.s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1).build();
-    }
-
-    private void insertObjectNonCompliant(String s3Folder) {
-        // Noncompliant: recreates AWS clients in each lambda invocation.
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1).build();
-        AmazonS3URI amazonS3URI = new AmazonS3URI(s3Folder);
-        s3Client.putObject(amazonS3URI.getBucket(), amazonS3URI.getKey(), "hello");
     }
 }
 // {/fact}
