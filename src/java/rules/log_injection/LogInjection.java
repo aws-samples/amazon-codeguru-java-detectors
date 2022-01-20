db16f4c843375c0ed8d4f9985736c889d9cb85e6
Nico Rosner <95888173+awsgaucho@users.noreply.github.com>
2022-01-20 18:27:58 -0500

    Add comment to log-injection sanitize method

--- src/java/rules/log_injection/LogInjection.java
+++ src/java/rules/log_injection/LogInjection.java
@@ -19,7 +19,7 @@ public class LogInjection {
         ModelAndView result = new ModelAndView("success");
         String userId = request.getParameter("userId");
         result.addObject("userId", userId);
-        // More logic to populate `result`.
+        // More logic here to populate the result.
         // Noncompliant: unsanitized input is logged.
         log.info("Successfully processed request for user ID: {}.", userId);
         return result;
@@ -33,13 +33,14 @@ public class LogInjection {
         String userId = request.getParameter("userId");
         String sanitizedUserId = sanitize(userId);
         result.addObject("userId", sanitizedUserId);
-        // More logic to populate `result`.
+        // More logic here to populate the result.
         // Compliant: input is sanitized before logging.
         log.info("Successfully processed request for user ID: {}.", sanitizedUserId);
         return result;
     }
 
     private static String sanitize(String userId) {
+        // Remove all chars except digits.
         return userId.replaceAll("\\D", "");
     }
     // {/fact}
