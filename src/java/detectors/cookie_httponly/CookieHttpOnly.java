9533e907eabbbe2761584faec1532d249c16ea05
Prajakta Ingle <prajaki@amazon.com>
2022-05-19 18:26:08 +0530

    Add compliant and noncompliant examples of java/cookie_httponly@v1.0

---
+++ src/java/detectors/cookie_httponly/CookieHttpOnly.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package detectors.cookie_httponly_rule;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+public class CookieHttpOnly{
+    // {fact rule=cookie_httponly@v1.0 defects=1}
+    public void CookieHttpOnlyNoncompliant(HttpServletResponse res) {
+        Cookie c = new Cookie("name", "not-null");
+        c.setSecure(true);
+        // Noncompliant: HttpOnly is not set before adding the cookie.
+        res.addCookie(c);
+    }
+    // {/fact}
+
+    // {fact rule=cookie_httponly@v1.0 defects=0}
+    public void CookieHttpOnlyCompliant(HttpServletResponse res) {
+        Cookie c = new Cookie("name", "not-null");
+        c.setSecure(true);
+        c.setHttpOnly(true);
+        // Compliant: HttpOnly is set before adding the cookie.
+        res.addCookie(c);
+    }
+    // {/fact}
+}
