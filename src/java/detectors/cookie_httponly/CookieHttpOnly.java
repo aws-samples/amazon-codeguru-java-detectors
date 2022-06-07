8d1d1246e7449a9febb67aa504bfda9108bf40a3
Prajakta Ingle <prajaki@amazon.com>
2022-06-07 14:58:57 +0530

    Addressed comments for cookie-httponly@v1.0

--- src/java/detectors/cookie_httponly/CookieHttpOnly.java
+++ src/java/detectors/cookie_httponly/CookieHttpOnly.java
@@ -10,21 +10,21 @@ import javax.servlet.http.Cookie;
 
 public class CookieHttpOnly{
     // {fact rule=cookie-httponly@v1.0 defects=1}
-    public void CookieHttpOnlyNoncompliant(HttpServletResponse res) {
-        Cookie c = new Cookie("name", "not-null");
-        c.setSecure(true);
+    public void cookieHttpOnlyNoncompliant(HttpServletResponse res) {
+        Cookie cookie = new Cookie("name", "not-null");
+        cookie.setSecure(true);
         // Noncompliant: HttpOnly is not set before adding the cookie.
-        res.addCookie(c);
+        res.addCookie(cookie);
     }
     // {/fact}
 
     // {fact rule=cookie-httponly@v1.0 defects=0}
-    public void CookieHttpOnlyCompliant(HttpServletResponse res) {
-        Cookie c = new Cookie("name", "not-null");
-        c.setSecure(true);
-        c.setHttpOnly(true);
+    public void cookieHttpOnlyCompliant(HttpServletResponse res) {
+        Cookie cookie = new Cookie("name", "not-null");
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
         // Compliant: HttpOnly is set before adding the cookie.
-        res.addCookie(c);
+        res.addCookie(cookie);
     }
     // {/fact}
 }
