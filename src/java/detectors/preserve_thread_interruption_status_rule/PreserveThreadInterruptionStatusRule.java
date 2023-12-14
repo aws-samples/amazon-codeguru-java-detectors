1d710051ec04c848dd4ae62e73e69c1d17c938d6
Riya <karmakri@amazon.com>
2023-12-14 20:59:07 +0530

    Add compliant and noncompliant examples of java/preserve-thread-interruption-status-rule@v1.0

--- src/java/detectors/preserve_thread_interruption_status_rule/PreserveThreadInterruptionStatusRule.java
+++ src/java/detectors/preserve_thread_interruption_status_rule/PreserveThreadInterruptionStatusRule.java
@@ -14,7 +14,7 @@ public class PreserveThreadInterruptionStatusRule {
     // {fact rule=preserve-thread-interruption-status-rule@v1.0 defects=1}
     public void preserveThreadInterruptionStatusRuleNoncompliant(int numTimes) throws RuntimeException {
         try {
-            for (int i; i < numTimes; i++ ) {
+            for (int i=0; i < numTimes; i++ ) {
                 Thread.sleep(1000L);
             }
         // Noncompliant: InterruptedException wrapped and rethrown using RuntimeException but without resetting interrupt status.
@@ -27,7 +27,7 @@ public class PreserveThreadInterruptionStatusRule {
     // {fact rule=preserve-thread-interruption-status-rule@v1.0 defects=0}
     public void preserveThreadInterruptionStatusRuleCompliant(int numTimes) throws RuntimeException {
         try {
-            for (int i; i < numTimes; i++ ) {
+            for (int i=0; i < numTimes; i++ ) {
                 Thread.sleep(1000L);
             }
         } catch (InterruptedException e) {
